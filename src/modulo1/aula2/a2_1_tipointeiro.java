97c3235fcd68afad5a0d0f1986d7d3a787b1d021
Talita Rosa <talisrosa4@gmail.com>
2021-09-10 16:59:37 -0300

    adding numeric data types

---
+++ src/modulo1/aula2/a2_1_tipointeiro.java
@@ -0,0 +1,11 @@
+package modulo1.aula2;
+
+public class a2_1_tipointeiro {
+
+    public static void main(String[] args) {
+        int numero1 = Integer.MAX_VALUE;
+        int numero2 = Integer.MIN_VALUE;
+        System.out.println(numero1);
+    }
+
+}
---
+++ src/modulo1/aula2/a2_5_float.java
@@ -0,0 +1,12 @@
+package modulo1.aula2;
+
+public class a2_5_float {
+
+    public static void main(String[] args) {
+        float menor_valor = Float.MIN_VALUE; // 1.4E-45
+        float maior_valor = Float.MAX_VALUE; // 3.4028235E38
+        float salario = 800.00f; //Cada vez que declarar uma variável do tipo float e atribuir um valor, colocar um f no final do valor, para diferenciar de double
+        System.out.println(menor_valor);
+        System.out.println(maior_valor);
+    }
+}
