c6f135085c33ecae6d63bd23cd28f39ec0358df6
Talita Rosa <talisrosa4@gmail.com>
2021-09-14 15:16:46 -0300

    create movie registration

--- src/modulo1/aula4/Maioridade.java
+++ src/modulo1/aula4/Maioridade.java
@@ -1,2 +1,24 @@
-package modulo1.aula4;public class Maioridade {
+package modulo1.aula4;
+
+import java.util.*;
+
+public class Maioridade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("*********** Cadastro de Pessoas ***********");
+        System.out.println("Nome:");
+        String nome = sc.nextLine();
+
+        System.out.println("Idade: ");
+        Short idade = sc.nextShort();
+
+        if (idade >= 18){
+            System.out.printf("%s, %d anos, foi cadastrada(o) com sucesso!", nome, idade);
+        }
+        else{
+            System.out.printf("Volte daqui a %d anos", (18 - idade));
+        }
+
+    }
 }
---
+++ src/pratica/Netflix.java
@@ -0,0 +1,56 @@
+package pratica;
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Netflix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-------------- Netflix - Cadastro --------------\n");
+        System.out.println("1- Filmes \t 2- Séries \t 3- Documentário \t 4- Sair \nDigite uma opção: ");
+        int opcao = sc.nextInt();
+        sc.nextLine();
+
+        switch (opcao){
+            case (1) :
+                System.out.println("Nome: ");
+                String movie = sc.nextLine();
+                System.out.println("Descrição: ");
+                String desc = sc.nextLine();
+                System.out.println("Genero: ");
+                String gender = sc.nextLine();
+                System.out.println("Ano de lançamento: ");
+                Short launchMovie = sc.nextShort();
+                sc.nextLine();
+
+                System.out.printf("Filme: %s, %s, %s, %d. Cadastrado com Sucesso!", movie, desc, gender, launchMovie);
+                break;
+            case(2) :
+                System.out.println("Nome: ");
+                String serie = sc.nextLine();
+                System.out.println("Ano de Lançamento: ");
+                Short launchSerie = sc.nextShort();
+                sc.nextLine();
+                System.out.println("Produtora: ");
+                String prod = sc.nextLine();
+
+                System.out.printf("Série: %s, %d, %s . Cadastrada com Sucesso!", serie, launchSerie, prod);
+                break;
+            case(3) :
+                System.out.println("Nome: ");
+                String doc = sc.nextLine();
+                System.out.println("Ano de lançamento: ");
+                Short launchDoc = sc.nextShort();
+                sc.nextLine();
+                System.out.println("Tema: ");
+                String theme = sc.nextLine();
+
+                System.out.printf("Documentário: %s, %d, %s . Cadastrado com Sucesso!", doc, launchDoc, theme);
+                break;
+            case(4) :
+                System.out.println("Tchau :)");
+                break;
+        }
+
+    }
+}
