d32ca212993964e63f987dca12b2213da40e3226
Talita Rosa <talisrosa4@gmail.com>
2021-09-20 11:20:26 -0300

    create monthly investment

---
+++ src/pratica/repeticao/Investimento.java
@@ -0,0 +1,11 @@
+package pratica.repeticao;
+
+public class Investimento {
+    public static void main(String[] args) {
+        double valor = 5000;
+        for (int i = 0; i < 24; i++) {
+            valor += (valor * 0.02);
+            System.out.printf("No %dº mês o retorno foi de %.2f  reais\n", i+1, valor);
+        }
+    }
+}
