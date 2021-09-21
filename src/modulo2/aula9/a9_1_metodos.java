36f2dc5c7eadaa0ddd2a69299d359cf5ddda1037
Talita Rosa <talisrosa4@gmail.com>
2021-09-21 15:50:00 -0300

    create calculator

--- src/modulo2/aula9/a9_1_metodos.java
+++ src/modulo2/aula9/a9_1_metodos.java
@@ -5,17 +5,7 @@ import java.util.*;
 
 public class a9_1_metodos {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Primeiro número: ");
-        int n1 = Integer.parseInt(sc.nextLine());
-        System.out.println("Segundo número: ");
-        int n2 = Integer.parseInt(sc.nextLine());
-
-        int r = sum(n1, n2);
-        System.out.println(r);
-    }
+    static Scanner sc = new Scanner(System.in);
 
     static void head(){
         System.out.println("----------------------------------------------------------");
@@ -28,10 +18,63 @@ public class a9_1_metodos {
         System.out.println("\t 2- Subtração");
         System.out.println("\t 3- Multilicação");
         System.out.println("\t 4- Divisão");
+        System.out.println("\t 5- Sair");
+    }
+
+    static void chooseMenu(){
+        int resultOp = 0;
+        int option = readNumber("Opção: ");
+        int number1 = readNumber("Primeiro número: ");
+        int number2 = readNumber("Segundo número: ");
+
+        switch (option){
+            case(1) :
+                resultOp = sum(number1, number2);
+                break;
+
+            case(2) :
+                resultOp = subtraction(number1, number2);
+                break;
+
+            case(3) :
+                resultOp = multiplication(number1, number2);
+                break;
+
+            case(4) :
+                resultOp = division(number1, number2);
+                break;
+
+            case(5) :
+                return;
+        }
+        System.out.printf("O resultado da operação é %d", resultOp);
+    }
+
+    static int readNumber(String message){
+        System.out.println(message);
+        int number = Integer.parseInt(sc.nextLine());
+        return number;
     }
 
     static int sum(int num1, int num2){
-        int result = num1 + num2;
-        return result;
+        return num1 + num2;
+    }
+
+    static int subtraction(int num1, int num2){
+        return num1 - num2;
+    }
+
+    static int multiplication(int num1, int num2){
+        return num1 * num2;
+    }
+
+    static int division(int num1, int num2){
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        head();
+        menu();
+        chooseMenu();
     }
 }
