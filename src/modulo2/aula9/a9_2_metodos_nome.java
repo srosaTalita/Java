b040b169b90a5072bd0899877a7537ca7b148b18
Talita Rosa <talisrosa4@gmail.com>
2021-09-21 16:28:35 -0300

    create print name

---
+++ src/modulo2/aula9/a9_2_metodos_nome.java
@@ -0,0 +1,12 @@
+package modulo2.aula9;
+
+public class a9_2_metodos_nome {
+
+    static void printName(String name, String surname){
+        System.out.printf("%s %s", name, surname);
+    }
+
+    public static void main(String[] args) {
+        printName("Talita", "Rosa");
+    }
+}
