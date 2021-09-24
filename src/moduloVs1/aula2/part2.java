a31cfb9fb180a129e8a311bd50ee6217b19ab406
Talita Rosa <talisrosa4@gmail.com>
2021-09-24 12:01:18 -0300

    add part 2 24/09

--- src/moduloVs1/aula2/part2.java
+++ src/moduloVs1/aula2/part2.java
@@ -1,5 +1,17 @@
 package moduloVs1.aula2;
 
 public class part2 {
-    
+    public static void main(String[] args) {        
+        byte idade = 17;
+
+        if(idade >= 18){
+            System.out.println("Pode consumir drogas");
+        }
+        if(idade > 18 || idade == 18){
+            System.out.println("Pode consumir drogas");
+        }
+        if(idade < 18){
+            System.out.println("Pode consumir coca");
+        }
+    }
 }
