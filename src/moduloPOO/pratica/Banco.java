3bd30a8ac0d350364dd99e18c7fdf331d349d2e2
Talita Rosa <talisrosa4@gmail.com>
2021-09-30 14:59:22 -0300

    add additional implementations on bank class

--- src/moduloPOO/pratica/Banco.java
+++ src/moduloPOO/pratica/Banco.java
@@ -1,5 +1,4 @@
 package moduloPOO.pratica;
-
 import java.util.Scanner;
 
 public class Banco {
@@ -11,35 +10,47 @@ public class Banco {
 
     static int menu(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("\n-------------\n1 - Saque\n2 - Transferência\n3 - Fechar conta\n-------------\n Escolha uma operação:");
+        System.out.println("\n-------------\n1 - Saque\n2 - Transferência\n3 - Depósito\n4 - Consultar Saldo\n5 - Fechar conta\n-------------\nEscolha uma operação:");
         int op = Integer.parseInt(sc.nextLine());
         return op;
     }
 
-    static void escolhaMenu(int opcao, CalculadoraTaxa ct){
+    static void escolhaMenu(int opcao, CalculadoraTaxa conta){
         double valor;
         int contador = 0;
-        while(opcao != 3){
+        while(opcao != 5){
             switch(opcao){
                 case 1:
                     contador += 1;
-                    valor = valor("Valor do Saque: ");
-                    System.out.printf("O valor debitado da conta foi: %.2f\n", ct.taxaSaque(valor, contador));
+                    valor = conta.valor("Valor do Saque: ");
+                    if(conta.verificaSaldo(valor)){
+                        System.out.printf("O valor debitado da conta foi: %.3f\n", conta.taxaSaque(valor, contador));
+                    }
+                    else{
+                        System.out.printf("Transação incompleta. Seu saldo é de %.3f reais", conta.saldo());
+                    }
                     break;
                 case 2:
-                    valor = valor("Valor da Transferência: ");
-                    System.out.printf("O valor debitado da conta foi: %.2f\n", ct.taxaTransferencia(valor));
+                    valor = conta.valor("Valor da Transferência: ");
+                    if(conta.verificaSaldo(valor)){
+                        System.out.printf("O valor debitado da conta foi: %.3f\n", conta.taxaTransferencia(valor));
+                    }
+                    else{
+                        System.out.printf("Transação incompleta. Seu saldo é de %.3f reais", conta.saldo());
+                    }        
+                    break;
+                case 3:
+                    valor = conta.valor("Valor do Depósito: ");
+                    conta.depositar(valor);
+                    System.out.println("Depósito feito com sucesso!");
+                    break;
+                case 4:
+                    System.out.printf("Seu saldo é de %.3f reais.", conta.saldo());
                     break;
             }
             opcao = menu();
         }
-        System.out.println("Conta fechada");
+        conta.fecharConta();
     }
 
-    static double valor(String mensagem){
-        Scanner sc = new Scanner(System.in);
-        System.out.println(mensagem);
-        double val = Double.parseDouble(sc.nextLine());
-        return val;
-    }
 }
--- src/moduloPOO/pratica/CalculadoraTaxa.java
+++ src/moduloPOO/pratica/CalculadoraTaxa.java
@@ -1,16 +1,60 @@
 package moduloPOO.pratica;
+import java.util.Scanner;
 
 public class CalculadoraTaxa {
-    
-    public Double taxaTransferencia(double valor){
-        double valorTaxa = (valor * 0.001);
-        return (valor + valorTaxa);
+
+    private double saldo;
+
+    public void fecharConta(){
+        System.out.println("Conta fechada.");
     }
 
-    public double taxaSaque(double valor, int contador){   
-        if(contador == 5)
-            return (valor + 1.30);
-        return valor;
+    public void depositar(double valor){
+        saldo += valor; 
+    }
+
+    public double saldo(){
+        return saldo;
+    }
+    
+    public Double taxaTransferencia(double valorTransf){
+        valorTransf += (valorTransf * 0.001);
+        saldo -= valorTransf;
+        return (valorTransf);
+    }
+
+    public double taxaSaque(double valorSaque, int contador){ 
+        if(contador % 5 == 0){
+            valorSaque += (valorSaque + 1.30);
+            saldo -= valorSaque;
+            return valorSaque;
+        }
+        saldo -= valorSaque;
+        return valorSaque;
+    }
+
+    public boolean verificaSaldo(double valor){
+        if(valor <= saldo())
+            return true;
+        return false;
+    }
+
+    static double valor(String mensagem){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(mensagem);
+        double val = Double.parseDouble(sc.nextLine());
+        
+        while(!validaValor(val)){
+            System.out.println("Valor Inválido! Digite novamente: ");
+            val = Double.parseDouble(sc.nextLine());
+        }
+        return val;
+    }
+
+    static boolean validaValor(double valor){
+        if(valor <= 0.0)
+            return false;
+        return true;
     }
 
 }
