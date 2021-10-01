2ccdf8c1ec43ea638454949460c5fa2f38c46dc2
Talita Rosa <talisrosa4@gmail.com>
2021-10-01 14:50:02 -0300

    add methods to calculate tax

--- src/moduloPOO/pratica/CalculoImposto.java
+++ src/moduloPOO/pratica/CalculoImposto.java
@@ -6,4 +6,18 @@ public class CalculoImposto {
     private double taxaIOF = 0.063;
     private double taxaIR = 0.15;
     
+    public double calculoISS(double valor){
+        double imposto = valor * taxaISS;
+        return imposto;
+    }
+
+    public double calculoIOF(double valor){
+        double imposto = valor * taxaIOF;
+        return imposto;
+    }
+
+    public double calculoIR(double valor){
+        double imposto = valor * taxaIR;
+        return imposto;
+    }
 }
