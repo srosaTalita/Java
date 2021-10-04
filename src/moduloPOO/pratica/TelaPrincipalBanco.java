2e76e799914dde581bf74e535ba6df7a7e52a2c7
Talita Rosa <talisrosa4@gmail.com>
2021-10-04 13:44:50 -0300

    print attributes of Banco class

--- src/moduloPOO/pratica/TelaPrincipalBanco.java
+++ src/moduloPOO/pratica/TelaPrincipalBanco.java
@@ -1,5 +1,18 @@
 package moduloPOO.pratica;
 
 public class TelaPrincipalBanco {
-    
+    public static void main(String[] args) {
+        ContaCorrente conta = new ContaCorrente();
+        conta.setCodigo("abc123");
+        conta.setAgencia(0001);
+        conta.setNumero(63587);
+        conta.setSaldo(10000.00);
+
+        String codigo = conta.getCodigo();
+        int agencia = conta.getAgencia();
+        int numero = conta.getNumero();
+        Double saldo = conta.getSaldo();
+
+        System.out.printf("Código: %s | Agência: %d | Número: %d | Saldo: %.2f reais.", codigo, agencia, numero, saldo);
+    }   
 }
