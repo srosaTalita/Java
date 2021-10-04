e0e10340f1432eefd85bae3eb9ed0eea4a16d09e
Talita Rosa <talisrosa4@gmail.com>
2021-10-04 17:12:46 -0300

    create array numeros

--- src/moduloColecoes/pratica/Numeros.java
+++ src/moduloColecoes/pratica/Numeros.java
@@ -1,5 +1,12 @@
 package moduloColecoes.pratica;
 
 public class Numeros {
-    
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = i;
+        }
+
+    }
 }
