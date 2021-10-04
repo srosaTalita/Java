8be056aa053713657f83066ee9c502d5ac884b7c
Talita Rosa <talisrosa4@gmail.com>
2021-10-04 17:09:57 -0300

    add categoria array

---
+++ src/moduloColecoes/ArrayCategorias.java
@@ -0,0 +1,30 @@
+package moduloColecoes;
+
+import java.util.Scanner;
+
+public class ArrayCategorias {
+
+    public static void main(String[] args) {
+        Categoria[] categorias = new Categoria[2];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < categorias.length; i++) {
+            System.out.println("Nome: ");
+            String nome = sc.next();
+
+            System.out.println("Descrição: ");
+            String desc = sc.next();
+            
+            Categoria c = new Categoria();
+            c.setNome(nome);
+            c.setNome(desc);
+
+            categorias[i] = c;
+        }
+        
+        for (Categoria cat : categorias) {
+            System.out.printf("%s - %s", cat.getNome(), cat.getDesc());
+        }
+    }
+
+}
---
+++ src/moduloColecoes/Categoria.java
@@ -0,0 +1,23 @@
+package moduloColecoes;
+
+public class Categoria {
+    
+    private String nome;
+    private String desc;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
