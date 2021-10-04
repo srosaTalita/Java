91c9d6180f4d49314e4f31316c2ca0f43f8b75ba
Talita Rosa <talisrosa4@gmail.com>
2021-10-04 13:38:59 -0300

    print attributes on Loja class

--- src/moduloPOO/pratica/TelaPrincipalLoja.java
+++ src/moduloPOO/pratica/TelaPrincipalLoja.java
@@ -4,6 +4,16 @@ public class TelaPrincipalLoja {
 
     public static void main(String[] args) {
         Produto produto = new Produto();
-        
+        produto.setCodigo("abc123");
+        produto.setDesc("60 polegadas - Full HD");
+        produto.setNome("Televisão");
+        produto.setValor(10000.00);
+
+        String codigo = produto.getCodigo();
+        String nome = produto.getNome();
+        String desc = produto.getDesc();
+        Double valor = produto.getValor();
+
+        System.out.printf("Código: %s | Produto: %s, %s, %.2f reais.", codigo, nome, desc, valor);
     }   
 }
