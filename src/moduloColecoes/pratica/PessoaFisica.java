c4d8b44fe7e45479d296bab06f8f42e84d684624
Talita Rosa <talisrosa4@gmail.com>
2021-10-04 17:18:32 -0300

    add attributes on PessoaFisica class

--- src/moduloColecoes/pratica/PessoaFisica.java
+++ src/moduloColecoes/pratica/PessoaFisica.java
@@ -1,5 +1,11 @@
 package moduloColecoes.pratica;
 
 public class PessoaFisica {
-
+    
+    private String codigo;
+    private String nome;
+    private String sobrenome;
+    private String rg;
+    private String cpf;
+    private int idade;
 }
