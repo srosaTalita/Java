f0710cbad5e8eca4756c0d1df8f785fd28ecf502
Talita Rosa <talisrosa4@gmail.com>
2021-10-04 17:22:05 -0300

    create PessoaFisica array

--- src/moduloColecoes/pratica/CadastroPessoas.java
+++ src/moduloColecoes/pratica/CadastroPessoas.java
@@ -1,5 +1,9 @@
 package moduloColecoes.pratica;
 
+import moduloPOO.aula4.Pessoa;
+
 public class CadastroPessoas {
-    
+    public static void main(String[] args) {
+        PessoaFisica[] pf = new PessoaFisica[5];
+    }
 }
