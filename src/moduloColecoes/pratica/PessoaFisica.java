1ec5a4bd78825c756ef68333cd5d874968a0a9ec
Talita Rosa <talisrosa4@gmail.com>
2021-10-04 17:19:48 -0300

    create getters and setters of PessoaFisica class

--- src/moduloColecoes/pratica/PessoaFisica.java
+++ src/moduloColecoes/pratica/PessoaFisica.java
@@ -1,11 +1,60 @@
 package moduloColecoes.pratica;
 
+import java.util.Set;
+
 public class PessoaFisica {
-    
+
     private String codigo;
     private String nome;
     private String sobrenome;
     private String rg;
     private String cpf;
     private int idade;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
