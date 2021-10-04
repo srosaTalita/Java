85c1a6abe68443b176a78e1707b8c22e9b9dd4ff
Talita Rosa <talisrosa4@gmail.com>
2021-10-04 13:28:54 -0300

    add getters and setters of contacorrente class

--- src/moduloPOO/pratica/ContaCorrente.java
+++ src/moduloPOO/pratica/ContaCorrente.java
@@ -2,4 +2,40 @@ package moduloPOO.pratica;
 
 public class ContaCorrente {
      
+    private int agencia;
+    private int numero;
+    private String codigo;
+    private Double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
 }
