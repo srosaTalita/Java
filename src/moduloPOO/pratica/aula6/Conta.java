ef8076a6bf8803798bb256615e6e0ecb44c2be71
Talita Rosa <talisrosa4@gmail.com>
2021-10-06 13:46:26 -0300

    create Conta class

---
+++ src/moduloPOO/pratica/aula6/Conta.java
@@ -0,0 +1,23 @@
+package moduloPOO.pratica.aula6;
+
+public class Conta {
+    
+    private Double saldo;
+    private String codigoCliente;
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(String codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+}
