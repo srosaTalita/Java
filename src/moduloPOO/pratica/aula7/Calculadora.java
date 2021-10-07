a6e387123ff63987169dd9a7fc4a94c5dae2f29d
Talita Rosa <talisrosa4@gmail.com>
2021-10-07 16:34:59 -0300

    add TelaCalculadora class

---
+++ src/moduloPOO/pratica/aula7/Calculadora.java
@@ -0,0 +1,21 @@
+package moduloPOO.pratica.aula7;
+
+public class Calculadora {
+    
+    public int soma(int n1, int n2){
+        return n1 + n2;
+    }
+    
+    public Double soma(Double n1, Double n2){
+        return n1 + n2;
+    }
+    
+    public float soma(float n1, float n2, float n3){
+        return n1 + n2 + n3;
+    }
+
+    public long soma(long n1, long n2, long n3){
+        return n1 + n2 + n3;
+    }
+
+}
