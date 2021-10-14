2c8007dea247cd58498f5df3a7ecd5d103a18bd9
Talita Rosa <talisrosa4@gmail.com>
2021-10-14 15:53:07 -0300

    add view

--- src/MVC/view/ProdutoView.java
+++ src/MVC/view/ProdutoView.java
@@ -7,7 +7,7 @@ public class ProdutoView {
     
     public static void main(String[] args) {
         
-        Produto p = new Produto("Tv", 1999.99);
+        Produto p = new Produto("Tv", 2800d);
 
         ProdutoController controller = new ProdutoController();
         controller.salvar(p);
