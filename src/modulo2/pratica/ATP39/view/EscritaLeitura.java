19cc29b1b73aa8b5c734eeabfe7f037b541990d6
Talita Rosa <talisrosa4@gmail.com>
2021-11-04 10:22:46 -0300

    add ATP39 - java.io

---
+++ src/modulo2/pratica/ATP39/view/EscritaLeitura.java
@@ -0,0 +1,35 @@
+package view;
+
+import java.io.*;
+
+public class EscritaLeitura {
+    
+    public static void main(String[] args) {
+        
+        try {
+            InputStream fis = System.in;
+            Reader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+
+            OutputStream fos = new FileOutputStream("ATP39/dados/mercado.txt");
+            Writer osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            String linha = "";
+            while(linha != null && !linha.equals("s")){
+                linha = br.readLine();
+                bw.write(linha);
+                bw.newLine();
+            }
+            bw.close();
+            br.close();
+
+        } 
+        catch (FileNotFoundException f) {
+            System.out.println("Arquivo não encontrado :/");
+        } 
+        catch (IOException e) {
+           System.out.println("Não foi possível escrever no arquivo :/");
+        }
+    }
+}
