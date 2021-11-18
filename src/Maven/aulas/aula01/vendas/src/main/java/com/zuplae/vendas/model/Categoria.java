500cb4f6f8a10532281e62d3d0eab32fbbf4bff0
Talita Rosa <talisrosa4@gmail.com>
2021-11-18 10:14:15 -0300

    add class Maven + Eclipse

---
+++ src/Maven/aulas/aula01/vendas/src/main/java/com/zuplae/vendas/model/Categoria.java
@@ -0,0 +1,33 @@
+package com.zuplae.vendas.model;
+
+public class Categoria {
+
+	private int id;
+	private String nome;
+	private String descricao;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nome=" + nome + ", descricao=" + descricao + "]";
+	}
+	
+}
