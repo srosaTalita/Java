cb0a6f21e67ea079dd478ad91df9ad2249b04186
Talita Rosa <talisrosa4@gmail.com>
2021-11-26 14:02:07 -0300

    add class - JPA + BaseRepository

--- Maven/aulas/aula11/banco/src/main/java/com/rosa/repository/CategoriaRepository.java
+++ Maven/aulas/aula11/banco/src/main/java/com/rosa/repository/CategoriaRepository.java
@@ -4,39 +4,18 @@ public class CategoriaDao {
 
     private EntityManager entityManager;
 
-    public CategoriaDao(){
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("banco");
-        this.entityManager = factory.createEntityManager();
-    }
-    
-    public int create(Categoria model)
-    {
-        this.entityManager.getTransaction().begin();
-        this.entityManager.persist(model);
-        this.entityManager.getTransaction().commit();
-    }
-
-    public List<Categoria> read()
-    {
-        return this.EntityManager
+    public List<Categoria> read(){
+        return this.getEntityManager()
             .createQuery("SELECT c FROM Categoria c", Categoria.class)
             .getResultList();
     }
-
-    public void update()
-    {
-        this.entityManager.getTransaction().begin();
-        this.entityManager.merge(model);
-        this.entityManager.getTransaction().commit();
-    }
-
-    public void delete(int id)
-    {
-        Categoria model = this.entityManager.find(Categoria.class, id);
+    
+    public void delete(int id){
+        Object model = this.getEntityManager().find(Categoria.class, id);
         if(model != null){
-            this.entityManager.getTransaction().begin();
-            this.entityManager.remove(model);
-            this.entityManager.getTransaction().commit();
-        }
+            this.getEntityManager().getTransaction().begin();
+            this.getEntityManager().remove(model);        
+            this.getEntityManager().getTransaction().commit();
+        }        
     }
 }
--- Maven/aulas/aula11/banco/src/main/java/com/rosa/repository/ContaCorrenteRepository.java
+++ Maven/aulas/aula11/banco/src/main/java/com/rosa/repository/ContaCorrenteRepository.java
@@ -8,28 +8,12 @@ import com.zuplae.model.ContaCorrente;
 public class ContaCorrenteRepository {
     private EntityManager entityManager;
 
-    public ContaCorrenteDao() {
-        this.entityManager = new ConnectionFactory().getConnection();     
-    }
-
-    public int create(ContaCorrente model){
-        this.entityManager.getTransaction().begin();
-        this.entityManager.persist(model);        
-        this.entityManager.getTransaction().commit();
-        return model.getId();
-    }
-
     public List<ContaCorrente> read(){
         return this.entityManager
             .createQuery("SELECT cc FROM ContaCorrente cc", ContaCorrente.class)
             .getResultList();
     }
 
-    public void update(ContaCorrente model){
-        this.entityManager.getTransaction().begin();
-        this.entityManager.merge(model);        
-        this.entityManager.getTransaction().commit();
-    }
     public void delete(int id){
         ContaCorrente model = this.entityManager.find(ContaCorrente.class, id);
         if(model != null){
