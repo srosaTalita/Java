ff40723aa8c755405b43dff5574963adb9f7563c
Talita Rosa <talisrosa4@gmail.com>
2021-11-29 17:19:40 -0300

    add ATP64 - Spring + MVC | part2

--- Spring/praticas/ATP64/filmes/src/main/java/com/rosa/filmes/controller/ClientesController.java
+++ Spring/praticas/ATP64/filmes/src/main/java/com/rosa/filmes/controller/ClientesController.java
@@ -8,6 +8,7 @@ import com.rosa.filmes.repository.ClientesRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -31,10 +32,22 @@ public class ClientesController {
         return "clientes-create";
     }
 
+    @GetMapping("/clientes/editar/{id}")
+    public String update(@PathVariable int id, Model req){
+        Clientes model = repository.findById(id).get();
+        req.addAttribute("cliente", model);
+        return "clientes-edit";
+    }
+
     @PostMapping("/clientes/salvar")
     public String salvar(Clientes model){
         repository.save(model);
         return "redirect:/clientes";
     }
 
+    @GetMapping("/clientes/deletar/{id}")
+    public String delete(@PathVariable int id){
+        repository.deleteById(id);
+        return "redirect:/clientes";
+    }
 }
